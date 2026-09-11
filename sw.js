0492cfd3d046837244a17361b7d2fd0abb70800a
MestreRofran <mestrerofran@gmail.com>
2026-09-11 02:35:14 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'meuappfinanceiro-v3.0';
+const APP_SHELL = ['./', './MeuAPPFinanceiro.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached =>
+        cached ||
+        fetch(event.request)
+          .then(response => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(cache =>
+              cache.put(event.request, copy)
+            );
+            return response;
+          })
+          .catch(() => caches.match('./MeuAPPFinanceiro.html'))
+      )
+  );
+});
